53ca9041d69bfe70d93a167cd69f81365592a6c5
Ashvin Upadhyay <64725443+ashvinupadhyay1132@users.noreply.github.com>
2024-10-04 15:26:10 +0530

    game.js
    Add highest score feature

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -6,47 +5,52 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
+var highestScore = localStorage.getItem('highestScore') || 0;
 
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
+    $("#highest-score").text("Highest Score: " + highestScore);
     nextSequence();
     started = true;
   }
 });
 
 $(".btn").click(function() {
-
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-    } else {
-      playSound("wrong");
-      $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
-      setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      startOver();
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
     }
-}
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
 
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    if (level > highestScore) {
+      highestScore = level;
+      localStorage.setItem('highestScore', highestScore);
+      $("#highest-score").text("Highest Score: " + highestScore);
+    }
+
+    startOver();
+  }
+}
 
 function nextSequence() {
   userClickedPattern = [];
@@ -62,7 +66,7 @@ function nextSequence() {
 
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
+  setTimeout(function() {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
